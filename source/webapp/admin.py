4b349725e6a366c776dcac7a9c69f9cd5c7d8887
Zhantorouulu_Kubat <78kuba00@gmail.com>
2023-01-14 12:40:46 +0600

    step 1

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Product, Review
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'review_text', 'rating', 'is_reviewed', 'created_at', 'updated_at')
+    list_filter = ('is_reviewed', 'product')
+    search_fields = ('author_name', 'review_text')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review, ReviewAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from datetime import datetime
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+CATEGORIES_CHOICES = [("other", "Разное"), ("electronics", "Электроника"), ("books", "Книги"),
+                      ("stationery", "Канцтовары")]
+class Product(models.Model):
+    name = models.CharField(max_length=255, blank=False, verbose_name='Название')
+    category = models.CharField(max_length=255, blank=False, choices=CATEGORIES_CHOICES, default=CATEGORIES_CHOICES[0][0], verbose_name='Категория')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    image = models.ImageField(blank=True, verbose_name='Картинка')
+
+    def __str__(self):
+        return f'{self.pk}. {self.name}'
+
+class Review(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reviews', verbose_name='Автор')
+    product = models.ForeignKey('webapp.Product', on_delete=models.CASCADE, verbose_name='Товар')
+    review_text = models.TextField(max_length=2000, blank=False, verbose_name='Текст отзыва')
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], blank=False, verbose_name='Оценка')
+    is_reviewed = models.BooleanField(default=False, verbose_name='С отзывом')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return f'{self.id}. {self.author}'
